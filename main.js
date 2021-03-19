12bed480036e3dafc6efbf13bc97fce310ddb122
developermayank-bit <77110719+developermayank-bit@users.noreply.github.com>
2021-03-19 16:28:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.progress-value > span').each(function(){
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 3500,
+            easing: 'swing',
+            step: function (now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
+
+
+	 
